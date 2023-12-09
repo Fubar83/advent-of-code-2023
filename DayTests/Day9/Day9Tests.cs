5ab8188008768ec17aff4d3abdc0037de9411dcc
HenrikJohansson <fubar83@gmail.com>
2023-12-09 08:09:17 +0100

    Add Day 9

---
+++ DayTests/Day9/Day9Tests.cs
@@ -0,0 +1,54 @@
+﻿using DayTests.Shared;
+using Shouldly;
+
+namespace DayTests.Day9;
+
+public class Day9Tests
+{
+    [Theory]
+    [InlineData("data.txt", 2043677056)]
+    [InlineData("example.txt", 114)]
+    public void TestStep1(string testFile, long expectedResult)
+    {
+        long result = 0;
+        foreach (var line in ResourceHelper
+                     .ForAssembly<Day9Tests>()
+                     .ReadLines(testFile))
+        {
+            result += Implementations.Part1(line);
+        }
+
+        result.ShouldBe(expectedResult);
+    }
+
+    [Theory]
+    [InlineData("data.txt", 1062)]
+    [InlineData("example.txt", 2)]
+    public void TestStep2(string testFile, long expectedResult)
+    {
+        long result = 0;
+        foreach (var line in ResourceHelper
+                     .ForAssembly<Day9Tests>()
+                     .ReadLines(testFile))
+        {
+            result += Implementations.Part2(line);
+        }
+
+        result.ShouldBe(expectedResult);
+    }
+
+    private static class Implementations
+    {
+        public static long Part1(string input)
+        {
+            var numbers = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+            return Extrapolate.CalculateNextValue(numbers);
+        }
+
+        public static long Part2(string input)
+        {
+            var numbers = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+            return Extrapolate.CalculatePreviousValue(numbers);
+        }
+    }
+}
---
+++ Shared.Math/Extrapolate.cs
@@ -0,0 +1,82 @@
+﻿using System.Numerics;
+
+namespace DayTests.Shared;
+
+public static class Extrapolate
+{
+    public static T CalculateNextValue<T>(IReadOnlyCollection<T> values) where T : INumber<T>
+    {
+        var rows = InitializeRows(values);
+
+        while (HasNonZeroValues(rows.Last()))
+        {
+            var lastRow = rows.Last();
+            var nextRow = CalculateNextRow(lastRow);
+            rows.Add(nextRow);
+        }
+
+        CalculateFinalValues(rows);
+
+        return rows[0].Last();
+    }
+
+    public static T CalculatePreviousValue<T>(IReadOnlyCollection<T> values) where T : INumber<T>
+    {
+        var rows = InitializeRows(values);
+
+        while (HasNonZeroValues(rows.Last()))
+        {
+            var lastRow = rows.Last();
+            var nextRow = CalculateNextRow(lastRow);
+            rows.Add(nextRow);
+        }
+
+        rows.Last().Add(T.Zero);
+        CalculatePreviousValues(rows);
+
+        return rows[0].First();
+    }
+
+    private static List<List<T>> InitializeRows<T>(IReadOnlyCollection<T> values) where T : INumber<T>
+    {
+        return new List<List<T>> { values.ToList() };
+    }
+
+    private static bool HasNonZeroValues<T>(List<T> row) where T : INumber<T>
+    {
+        return row.Any(x => x != T.Zero);
+    }
+
+    private static List<T> CalculateNextRow<T>(List<T> lastRow) where T : INumber<T>
+    {
+        var next = new List<T>();
+        for (var i = 0; i < lastRow.Count - 1; i++)
+        {
+            next.Add(lastRow[i + 1] - lastRow[i]);
+        }
+
+        return next;
+    }
+
+    private static void CalculateFinalValues<T>(List<List<T>> rows) where T : INumber<T>
+    {
+        for (var i = rows.Count - 2; i >= 0; i--)
+        {
+            var row = rows[i];
+            var nextRow = rows[i + 1];
+            row.Add(row.Last() + nextRow.Last());
+        }
+    }
+
+    private static void CalculatePreviousValues<T>(List<List<T>> rows) where T : INumber<T>
+    {
+        for (var i = rows.Count - 2; i >= 0; i--)
+        {
+            var row = rows[i];
+            var nextRow = rows[i + 1];
+
+            var prev = row[0] - nextRow[0];
+            row.Insert(0, prev);
+        }
+    }
+}
